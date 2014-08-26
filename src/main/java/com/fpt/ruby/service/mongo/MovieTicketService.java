d05d87fe3460576905ffe1b7aa184240c221f484
Administrator <admin@local.host>
2014-08-26 17:58:13 +0700

    update query class

---
+++ src/main/java/com/fpt/ruby/service/mongo/MovieTicketService.java
@@ -0,0 +1,33 @@
+package com.fpt.ruby.service.mongo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.mongodb.core.MongoOperations;
+
+import com.fpt.ruby.model.MovieTicket;
+
+public class MovieTicketService {
+	private MongoOperations mongoOperations;
+	public MovieTicketService(MongoOperations mongoOperations){
+		this.mongoOperations = mongoOperations;
+	}
+	
+	public List<MovieTicket> findAll(){
+		return mongoOperations.findAll(MovieTicket.class);
+	}
+	
+	public List<MovieTicket> findMoviesMathCondition(MovieTicket matchMovieTicket){
+		List<MovieTicket> movieTickets = mongoOperations.findAll(MovieTicket.class);
+		List<MovieTicket> results = new ArrayList<MovieTicket>();
+		for (MovieTicket movieTicket : movieTickets){
+			if ( (matchMovieTicket.getCinema() == null || (movieTicket.getCinema().equals(matchMovieTicket.getCinema()))) 
+			 &&	 (matchMovieTicket.getCity() == null || (movieTicket.getCity().equals(matchMovieTicket.getCity())))
+			 &&  (matchMovieTicket.getMovie() == null || (movieTicket.getMovie().equals(matchMovieTicket.getMovie())))
+			 &&  (matchMovieTicket.getType() == null || (movieTicket.getType().equals(matchMovieTicket.getType())))
+			 &&  (matchMovieTicket.getDate() == null || (movieTicket.getDate().equals(matchMovieTicket.getDate()))) )
+		    results.add(movieTicket);
+		}
+		return results;
+	}
+}
