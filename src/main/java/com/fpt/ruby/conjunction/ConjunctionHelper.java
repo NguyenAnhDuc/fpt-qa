91b38b1010ff8e56951c55b99d3fcd5ef9f0a0f4
DungxNguyen <dungx.ngt@gmail.com>
2014-09-10 11:31:01 +0700

    Integrate AdditionalInformation v3

--- src/main/java/com/fpt/ruby/conjunction/ConjunctionHelper.java
+++ src/main/java/com/fpt/ruby/conjunction/ConjunctionHelper.java
@@ -1,3 +1,4 @@
+
 package com.fpt.ruby.conjunction;
 
 import java.io.File;
@@ -6,29 +7,32 @@ import java.util.List;
 import mdnlib.struct.pair.Pair;
 import modifier.ConjunctionWithType;
 
-
-
-public class ConjunctionHelper {
+public class ConjunctionHelper{
 	private ConjunctionWithType conjunctionWithType;
-	public ConjunctionHelper(){
-		 conjunctionWithType = new ConjunctionWithType();
-		 conjunctionWithType.loadConjunctionType( new File( "movies_infor.txt"));
-	}
-	public ConjunctionHelper(String dir){
-		 conjunctionWithType = new ConjunctionWithType();
-		 conjunctionWithType.loadConjunctionType( new File(dir + "/movies_infor.txt"));
-	}
-	
-	public List<Pair<String, String>> getConjunction(String text){
-		return conjunctionWithType.getRelevantConjunctionsWithType(text); 
-	}
-	
-	/*public List<Pair<String, String>> getConjunction(String text){
-		return conjunctionWithType.getOriginRelevantConjunctionsWithType(text); 
-	}*/
-	
-	/*public List<Pair<ArrayList<String>, String>> getListConjunction(String text){
-		return conjunctionWithType.getListRelevantConjunctionsWithType(text); 
-	}*/
 
- }
+	public ConjunctionHelper() {
+		conjunctionWithType = new ConjunctionWithType();
+		conjunctionWithType.loadConjunctionType( new File( "movies_infor.txt" ) );
+	}
+
+	public ConjunctionHelper( String dir ) {
+		conjunctionWithType = new ConjunctionWithType();
+		conjunctionWithType.loadConjunctionType( new File( dir + "/movies_infor.txt" ) );
+	}
+
+	public List< Pair< String, String >> getConjunction( String text ) {
+		return conjunctionWithType.getOriginRelevantConjunctionsWithType( text );
+	}
+
+	/*
+	 * public List<Pair<String, String>> getConjunction(String text){ return
+	 * conjunctionWithType.getOriginRelevantConjunctionsWithType(text); }
+	 */
+
+	/*
+	 * public List<Pair<ArrayList<String>, String>> getListConjunction(String
+	 * text){ return
+	 * conjunctionWithType.getListRelevantConjunctionsWithType(text); }
+	 */
+
+}
