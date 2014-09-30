4217b68a68a47b956d36b32b5862f4b61c2d4b66
ngandt <ngandt@example.com>
2014-09-30 17:45:57 +0700

    draft answer mapper

--- src/main/java/com/fpt/ruby/nlp/TVAnswerMapperImpl.java
+++ src/main/java/com/fpt/ruby/nlp/TVAnswerMapperImpl.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import com.fpt.ruby.helper.RedisHelper;
 import com.fpt.ruby.model.TVProgram;
+import com.fpt.ruby.service.TVProgramService;
 
 import fpt.qa.intent.detection.TVIntentDetection;
 import fpt.qa.mdnlib.util.string.DiacriticConverter;
@@ -17,32 +18,88 @@ public class TVAnswerMapperImpl implements TVAnswerMapper {
 	public static final String DEF_ANS = "Xin lỗi, chúng tôi không có thông tin cho câu trả lời của bạn";
 	private TVIntentDetection intentDetector = new TVIntentDetection();
 	private TVIntentDetection nonDiacritic = new TVIntentDetection();
+	private TVProgramService tps = new TVProgramService();
 	
 	public void init() {
-		String dir = (new RedisHelper()).getClass().getClassLoader().getResource("").getPath();
-		intentDetector.init( dir + "/dicts", dir + "/qc/tv" );
-		nonDiacritic.init( dir + "/dicts/non-diacritic", dir + "/qc/tv/non-diacritic" );
+//		String dir = (new RedisHelper()).getClass().getClassLoader().getResource("").getPath();
+		String dir = "/home/ngan/Work/AHongPhuong/RubyWeb/rubyweb/src/main/resources";
+		intentDetector.init( dir + "/qc/tv", dir + "/dicts");
+		nonDiacritic.init( dir + "/qc/tv/non-diacritic", dir + "/dicts/non-diacritic");
 	}
 	
 	public String getAnswer ( String question ) {
-		String tmp = "";
+		String tmp = "\t" + question + "\n";
 		
 		String intent = intentDetector.getIntent( question );
 		System.out.println("TV Intent: " + intent);
-		tmp += "TV Intent: " + intent + "\n";
+		tmp += "\t" + "TV Intent: " + intent + "\n";
 		
 		String intent2 = nonDiacritic.getIntent( question );
 		System.out.println("Non-diacritic TV Intent: " + intent2);
-		tmp += "Non-diacritic TV Intent: " + intent2 + "\n";
+		tmp += "\t" + "Non-diacritic TV Intent: " + intent2 + "\n";
 		
 		if (!DiacriticConverter.hasDiacriticAccents(question)){
 			intent = intent2;
 		}
+		if (intent.equals( "UDF")){
+			return tmp + DEF_ANS + "\n\n\n";
+		}
 		
-		TVModifiers mod = TVModifiers.getModifiers( question );
-		tmp += mod + "\n\n\n";
+		TVModifiers mod = TVModifiers.getModifiers( question.replaceAll("(\\d+)(h)", "$1 giờ ").replaceAll( "\\s+", " " ) );
+		tmp += "\t" + question.replaceAll("(\\d+)(h)", "$1 giờ ").replaceAll( "\\s+", " " ) + "\n";
+		tmp += "\t" + mod + "\n";
 		
-		return tmp;
+		List< TVProgram > progs = tps.getList( mod, question );
+
+		if (mod.getChannel() == null && mod.getProg_title() == null){
+			if (mod.getStart() == null){
+				return tmp + DEF_ANS + "\n\n\n";
+			}
+			if (mod.getStart().equals( mod.getEnd() )){
+				return tmp + getChannelAndProgram( progs ) + "\n\n\n";
+			}
+			return tmp + getChannelProgAndTime( progs ) + "\n\n\n";
+		}
+		
+		if (mod.getChannel() == null){
+			if (intent.equals( "DAT" ) || intent.equals( "POL" ) && progs.isEmpty()){
+				return tmp + "Không đúng!" + "\n\n\n";
+			}
+			if (mod.getStart() == null){
+				return tmp + DEF_ANS + "\n\n\n";
+			}
+			if (mod.getStart().equals( mod.getEnd() )){
+				if (intent.equals( "CHN" )){
+					return tmp + getChannel( progs ) + "\n\n\n";
+				}
+				return tmp + getChannelAndProgram( progs ) + "\n\n\n";
+			}
+			return tmp + getChannelProgAndTime( progs ) + "\n\n\n";
+		}
+		
+		if (mod.getProg_title() == null){
+			if (mod.getStart() == null){
+				return tmp + DEF_ANS + "\n\n\n";
+			}
+			if (mod.getStart().equals( mod.getEnd() )){
+				return tmp + getTitle( progs ) + "\n\n\n";
+			}
+			return tmp + getTitleAndTime( progs ) + "\n\n\n";
+		}
+		
+		if (mod.getStart() == null){
+			return tmp + DEF_ANS + "\n\n\n";
+		}
+		
+		if (mod.getStart().equals( mod.getEnd() )){
+			if (progs.isEmpty()){
+				return tmp + "Không có " + mod.getProg_title() + " nào trên kênh " + mod.getChannel() + " vào lúc đó!\n\n\n";
+			}
+			return tmp + getTitle( progs ) + "\n\n\n";
+		}
+		
+		return tmp + getTitleAndTime( progs ) + "\n\n\n";
+		
 //		return DEF_ANS;
 	}
 
@@ -56,7 +113,7 @@ public class TVAnswerMapperImpl implements TVAnswerMapper {
 			limit = progs.size();
 		}
 		for (int i = 0; i < limit; i++){
-			time += progs.get( i ).getStart_date() + ", ";
+			time += progs.get( i ).getStart_date() + "\n";
 		}
 		
 		return time.substring( 0, time.length() - 2 );
@@ -72,7 +129,7 @@ public class TVAnswerMapperImpl implements TVAnswerMapper {
 			limit = progs.size();
 		}
 		for (int i = 0; i < limit; i++){
-			title += progs.get( i ).getTitle() + ", ";
+			title += progs.get( i ).getTitle() + "\n";
 		}
 		
 		return title.substring( 0, title.length() - 2 );
@@ -88,7 +145,7 @@ public class TVAnswerMapperImpl implements TVAnswerMapper {
 			limit = progs.size();
 		}
 		for (int i = 0; i < limit; i++){
-			channel += progs.get( i ).getTitle() + ", ";
+			channel += progs.get( i ).getTitle() + "\n";
 		}
 		
 		return channel.substring( 0, channel.length() - 2 );
@@ -105,7 +162,7 @@ public class TVAnswerMapperImpl implements TVAnswerMapper {
 		}
 		for (int i = 0; i < limit; i++){
 			TVProgram tv = progs.get( i );
-			title += tv.getTitle() + " : " + tv.getStart_date().toString() + ", ";
+			title += tv.getTitle() + " : " + tv.getStart_date().toString() + "\n";
 		}
 		
 		return title.substring( 0, title.length() - 2 );
@@ -122,7 +179,7 @@ public class TVAnswerMapperImpl implements TVAnswerMapper {
 		}
 		for (int i = 0; i < limit; i++){
 			TVProgram tv = progs.get( i );
-			info += tv.getChannel() + " : " + tv.getTitle() + ", ";
+			info += tv.getChannel() + " : " + tv.getTitle() + "\n";
 		}
 		
 		return info.substring( 0, info.length() - 2 );
@@ -139,7 +196,7 @@ public class TVAnswerMapperImpl implements TVAnswerMapper {
 		}
 		for (int i = 0; i < limit; i++){
 			TVProgram tv = progs.get( i );
-			info += tv.getChannel() + " : " + tv.getStart_date() + ", ";
+			info += tv.getChannel() + " : " + tv.getStart_date() + "\n";
 		}
 		
 		return info.substring( 0, info.length() - 2 );
@@ -156,7 +213,7 @@ public class TVAnswerMapperImpl implements TVAnswerMapper {
 		}
 		for (int i = 0; i < limit; i++){
 			TVProgram tv = progs.get( i );
-			info += tv.getChannel() + " : " + tv.getTitle() + " : " + tv.getStart_date() + ", ";
+			info += tv.getChannel() + " : " + tv.getTitle() + " : " + tv.getStart_date() + "\n";
 		}
 		
 		return info.substring( 0, info.length() - 2 );
@@ -175,6 +232,11 @@ public class TVAnswerMapperImpl implements TVAnswerMapper {
 			BufferedReader reader = new BufferedReader( new FileReader( fileIn ) );
 			BufferedWriter writer = new BufferedWriter( new FileWriter( fileOut ) );
 			
+			String line;
+			while ((line = reader.readLine()) != null){
+				writer.write( getAnswer( line  ) + "\n");
+			}
+			
 			writer.close();
 			reader.close();
 		}catch ( IOException e ){
@@ -184,7 +246,10 @@ public class TVAnswerMapperImpl implements TVAnswerMapper {
 	}
 	
 	public static void main (String[] args){
-		
+		TVAnswerMapperImpl tam = new TVAnswerMapperImpl();
+		tam.init();
+		tam.studyFile( "/home/ngan/Work/AHongPhuong/Intent_detection/data/tv/AIML_tvd_questions.txt",
+				"/home/ngan/Work/AHongPhuong/Intent_detection/data/tv/AIML_tvd_questions.out" );
 	}
 
 }
