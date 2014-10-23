94dc5c43505be0a852d9ca35cb88f6605463b0c7
thucdx <thucdx31@gmail.com>
2014-10-23 09:38:19 +0700

    TypeRecognizer -initial commit

--- src/main/java/fpt/qa/type_mapper/ProgramType.java
+++ src/main/java/fpt/qa/type_mapper/ProgramType.java
@@ -1,49 +1,12 @@
+/**
+ * 
+ */
 package fpt.qa.type_mapper;
 
-import org.apache.commons.lang.enums.Enum;
-
-public class ProgramType {
-	
-	private EnumType type ;
-	private String channel ;
-	private String program ;
-	public ProgramType() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public ProgramType(EnumType type, String channel, String program) {
-		this.type = type ;
-		this.channel = channel;
-		this.program = program;
-	}
-
-	public EnumType getType() {
-		return type;
-	}
-
-	public void setType(EnumType type) {
-		this.type = type;
-	}
-
-	public String getChannel() {
-		return channel;
-	}
-
-	public void setChannel(String channel) {
-		this.channel = channel;
-	}
-
-	public String getProgram() {
-		return program;
-	}
-
-	public void setProgram(String program) {
-		this.program = program;
-	}
-
-	@Override
-	public String toString() {
-		return "ProgramType [type=" + type + ", channel=" + channel
-				+ ", program=" + program + "]";
-	}
+/**
+ * @author Bui Thien
+ *
+ */
+public enum ProgramType {
+	SPORT, FILM, DOC_FILM, MUSIC, GAME_SHOW, MULTI_MEDIA, NEW, FASHION, CARTOON, ALL
 }
--- src/main/java/fpt/qa/type_mapper/TypeMapper.java
+++ src/main/java/fpt/qa/type_mapper/TypeMapper.java
@@ -1,5 +1,6 @@
 package fpt.qa.type_mapper;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -7,15 +8,38 @@ import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
 
+import org.jsoup.nodes.Document;
+
 import vn.hus.nlp.utils.UTF8FileUtility;
 
 public class TypeMapper {
+	static List<TypeRecognizer> types;
+	private static Map<ProgramType, Set<String>> typeMapper;// = new
+															// HashMap<EnumType,
+	// Set<String>>();
+	private static final double NOT_SURE = 0.3;
+	private static final double WITH_QUOTE_RATIO = 0.7;
+	private static final double RANGE = 3;
 
-	private Map<EnumType, Set<String>> typeMapper;// = new HashMap<EnumType,
-													// Set<String>>();
+	static {
+		types = new ArrayList<TypeRecognizer>();
+		types.add(new FilmTypeRecognizer());
+		types.add(new GameShowTypeRecognizer());
+		types.add(new MusicTypeRecognizer());
+		types.add(new SportTypeRecognizer());
+		
+		for (TypeRecognizer tp : types) {
+			tp.show();
+			System.out.println("\n________________");
+		}
+		loadData("resourcesc/type_mapper.txt");
+	}
 
-	public TypeMapper(String fileName) {
-		typeMapper = new HashMap<EnumType, Set<String>>();
+	public TypeMapper() {
+	}
+	
+	private static void loadData(String fileName) {
+		typeMapper = new HashMap<ProgramType, Set<String>>();
 		String[] lines = UTF8FileUtility.getLines(fileName);
 		for (String line : lines) {
 			if (line == "" && line.charAt(0) == '#') {
@@ -33,59 +57,115 @@ public class TypeMapper {
 			typeMapper.put(getType(type), set);
 		}
 	}
-	
-	
-	public EnumType getType(String channel, String program) {
+
+	public TypeMapper(String fileName) {
+		loadData(fileName);
+	}
+
+	public ProgramType getType(String channel, String program) {
 		channel = channel.trim();
 		program = program.trim();
-		for( EnumType type : typeMapper.keySet() ){
-			if(typeMapper.get(type).contains(channel) || typeMapper.get(type).contains(program)) {
+
+		System.out.println("STEP 1");
+		// step 1 : based on prepared data
+		for (ProgramType type : typeMapper.keySet()) {
+			if (typeMapper.get(type).contains(channel)
+					|| typeMapper.get(type).contains(program)) {
 				return type;
 			}
 		}
-		return EnumType.ALL;
+
+		System.out.println("STEP 2");
+		// step 2 : based on keywords
+		Double maxScore = 0.0;
+		ProgramType type = null;
+
+		for (TypeRecognizer tp : types) {
+			Double confidentLv = tp.belongThisType(channel, program);
+			if (confidentLv > maxScore) {
+				maxScore = confidentLv;
+				type = tp.getType();
+			}
+			System.out.println(tp.getType() + " " + confidentLv);
+		}
+
+		if (maxScore > NOT_SURE) {
+			return type;
+		}
+
+		System.out.println("STEP 3");
+		// step 3 : ba.fi
+		try {
+			String doc1 = TypeMapperUtil.getGoogleSearch(program, true);
+			String doc2 = TypeMapperUtil.getGoogleSearch(program, false);
+
+			TypeRecognizer result = null;
+			Double max = 0.0;
+
+			for (TypeRecognizer tp : types) {
+				int a1 = TypeMapperUtil.getTotalWord(doc1, tp);
+				int a2 = TypeMapperUtil.getTotalWord(doc2, tp);
+
+				Double score = WITH_QUOTE_RATIO * a1 + a2;
+				System.out.println(tp.getType() + " " + a1 + " " + a2
+						+ " score = " + score);
+
+				if (score > max) {
+					max = score;
+					result = tp;
+				}
+			}
+
+			if (max > RANGE)
+				return result.getType();
+		} catch (Exception ex) {
+			System.out.println("ERRO " + ex.getMessage());
+		}
+
+		return ProgramType.ALL;
 	}
-	
-	public Set<String> getContent(EnumType type) {
+
+	public Set<String> getContent(ProgramType type) {
 		return typeMapper.get(type);
 	}
-	
+
 	// Test
 	void print() {
-		System.out.println( "size = " + typeMapper.size() );
-		for( EnumType word : typeMapper.keySet() ){
-			System.out.print( word + "\t" );
-			Set< String > syns = typeMapper.get( word );
-			System.out.println( syns.toString() );
+		System.out.println("size = " + typeMapper.size());
+		for (ProgramType word : typeMapper.keySet()) {
+			System.out.print(word + "\t");
+			Set<String> syns = typeMapper.get(word);
+			System.out.println(syns.toString());
 		}
 	}
-	private EnumType getType(String type) {
+
+	private static ProgramType getType(String type) {
 		// TODO Auto-generated method stub
 		switch (type) {
 		case "SPORT":
-			return EnumType.SPORT;
+			return ProgramType.SPORT;
 		case "CARTOON":
-			return EnumType.CARTOON;
+			return ProgramType.CARTOON;
 		case "DOC_FILM":
-			return EnumType.DOC_FILM;
+			return ProgramType.DOC_FILM;
 		case "FASHION":
-			return EnumType.FASHION;
+			return ProgramType.FASHION;
 		case "GAME_SHOW":
-			return EnumType.GAME_SHOW;
+			return ProgramType.GAME_SHOW;
 		case "NEW":
-			return EnumType.NEW;
+			return ProgramType.NEW;
 		case "MULTI_MEDIA":
-			return EnumType.MULTI_MEDIA;
+			return ProgramType.MULTI_MEDIA;
 		case "FILM":
-			return EnumType.FILM;
+			return ProgramType.FILM;
 		default:
-			return EnumType.ALL;
+			return ProgramType.ALL;
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		TypeMapper map = new TypeMapper( "resourcesc/type_mapper.txt" );
+		TypeMapper map = new TypeMapper("resourcesc/type_mapper.txt");
 		map.print();
 	}
-	
+
 }
